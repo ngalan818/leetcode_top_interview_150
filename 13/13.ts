f07d894a01b435207641e1c922ad97eb5f8549e6
Alan Ng <alan.ng@ibm.com>
2024-07-16 12:52:40 -0400

    13 solved

---
+++ 13/13.ts
@@ -0,0 +1,53 @@
+function romanToInt(s: string): number {
+    let ans: number = 0;
+    for (let i = 0; i < s.length; i++) {
+        switch (s.charAt(i)) {
+            case "M":
+                ans += 1000;
+                break;
+            case "D":
+                ans += 500;
+                break;
+            case "C":
+                if (i < s.length - 1 && s.charAt(i+1) == "D") {
+                    ans += 400;
+                    i++;
+                } else if (i < s.length - 1 &&  s.charAt(i+1) == "M") {
+                    ans += 900;
+                    i++;
+                } else {
+                    ans += 100;
+                }
+                break;
+            case "L":
+                ans += 50;
+                break;
+            case "X":
+                if (i < s.length - 1 && s.charAt(i+1) == "L") {
+                    ans += 40;
+                    i++;
+                } else if (i < s.length - 1 &&  s.charAt(i+1) == "C") {
+                    ans += 90;
+                    i++;
+                } else {
+                    ans += 10;
+                }
+                break;
+            case "V":
+                ans += 5;
+                break;
+            case "I":
+                if (i < s.length - 1 && s.charAt(i+1) == "V") {
+                    ans += 4;
+                    i++;
+                } else if (i < s.length - 1 &&  s.charAt(i+1) == "X") {
+                    ans += 9;
+                    i++;
+                } else {
+                    ans += 1;
+                }
+                break;
+        }
+    }
+    return ans;
+};
