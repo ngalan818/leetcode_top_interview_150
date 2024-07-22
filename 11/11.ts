badce1011030b57f5263f838992839e533777f31
Alan Ng <alan.ng@ibm.com>
2024-07-22 15:26:14 -0400

    11 solved

---
+++ 11/11.ts
@@ -0,0 +1,16 @@
+function maxArea(height: number[]): number {
+    let i = 0;
+    let j = height.length - 1;
+
+    let max = 0;
+    while (i < j) {
+        if (height[i] < height[j]) {
+            max = Math.max(max, height[i] * (j - i));
+            i++;
+        } else {
+            max = Math.max(max, height[j] * (j - i));
+            j--;
+        }
+    }
+    return max;
+};
